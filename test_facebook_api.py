924b020d01abea582f01696c3cda6ce8f8e72844
Cícero Henrique <cicero_penha@hotmail.com>
2020-03-26 22:32:18 -0300

    Improve

--- test_facebook_api.py
+++ test_facebook_api.py
@@ -3,17 +3,51 @@ class backup:
     def write_in_file(self, profile):
         f = open("backupFile.txt", "w")
         f.write("name:" + profile["name"] + "\n")
-        f.write("work:" + profile["work"] + "\n")
-        f.write("study:" + profile["study"] + "\n")
-        f.write("cities:" + profile["hometown"] + "\n")
-        f.write("words:" + profile["words"] + "\n")
+        
+        word = ""
+        for work in profile["work"]:
+            word = word + work + "; " 
+        f.write("work:" + word + "\n")
+
+        word = ""
+        for study in profile["study"]:
+            word = word + study + "; " 
+        f.write("study:" + word + "\n")
+
+        word = ""
+        for city in profile["cities"]:
+            word = word + city + "; "
+        f.write("cities:" + word + "\n")
+
+        word = ""
+        for i in range(0, len(profile["words"])):
+            try:
+                word = word + profile["words"][i] + "; "
+            except UnicodeEncodeError:
+                print("skip")
+        
+        
+        
+        # WARNING
+        
+        f.write("words:" + word.encode("utf-8") + "\n")
+        
+        # WARNING
+        
+        
+        
+        
+        
         f.close()
 
     def read_from_file(self):
         profile = []
         f = open("backupFile.txt", "r")
         for line in f:
-            profile[line.split(":")[0]] = line.split(":")[1]
+            type = line.split(":")[0]
+            content = line.split(":")[1]
+            content =  content.split("; ")
+            profile[type] = content
 
         return profile
 
