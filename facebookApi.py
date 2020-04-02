ce77bbbf5b010a3685421be30d13504c35614d1d
Cicero Henrique <cicero.henrique@inatel.br>
2020-04-02 09:45:17 -0300

    Remove cupp

--- facebookApi.py
+++ facebookApi.py
@@ -1,13 +1,11 @@
 
 # from facebook_scraping import facebook_scraping as fs
-# from cupp import cupp
 from backup import backup
 from generate_passwords import generate_passwords
 
 URL = ("https://www.facebook.com/zuck")
 
 # profile = fs.scraping(URL)
-#cupp(profile)
 profile = backup()._profile
 # backup(profile)
 generate_passwords(profile)
