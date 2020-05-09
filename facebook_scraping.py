3d7f993646af199779e4aaa1759f8b470bee2612
Cicero Henrique <cicero_penha@hotmail.com>
2020-05-09 20:49:12 -0300

    Improve the scraping

--- facebook_scraping.py
+++ facebook_scraping.py
@@ -22,30 +22,41 @@ class facebook_scraping:
         name = soup.find("div", {"class": "_2nlj _2xc6"})
         name = name.find("a")
         name = name.get_text()
-        study_work = soup.find_all("div", {"class": "_4qm1"})
-        work = study_work[0].find_all("div", {"class": "_2lzr _50f5 _50f7"})
-        study = study_work[1].find_all("div", {"class": "_2lzr _50f5 _50f7"})
-        cities = soup.find_all("span", {"class": "_2iel _50f7"})
+        print(name)
+        profile["name"] = name
+
+        info = soup.find_all("div", {"class": "_4qm1"})
+        labels = soup.find_all("div", {"class": "clearfix _h71"})
+        for i in range(0, len(labels)):
+            label = labels[i].get_text()
+            if(label.lower() == "trabalho"):
+                print("\t\t " + str(i) + " - " + label)
+                work = info[i].find_all("div", {"class": "_2lzr _50f5 _50f7"})
+                profile["work"] = []
+                for i in range(0, len(work)):
+                    print(work[i].a.get_text())
+                    profile["work"].append(work[i].a.get_text())
+
+            if(label.lower() == "educação"):
+                print("\t\t " + str(i) + " - " + label)
+                study = info[i].find_all("div", {"class": "_2lzr _50f5 _50f7"})
+                profile["study"] = []
+                for i in range(0, len(study)):
+                    print(study[i].a.get_text())
+                    profile["study"].append(study[i].a.get_text())
+
+            if(label.lower() == "cidade atual e cidade natal"):
+                print("\t\t " + str(i) + " - " + label)
+                cities = info[i].find_all("span", {"class": "_2iel _50f7"})
+                profile["cities"] = []
+                for i in range(0, len(cities)):
+                    print(cities[i].a.get_text())
+                    profile["cities"].append(cities[i].a.get_text())
+
         table_favorites = soup.find("table", {"class": "mtm _5e7- profileInfoTable _3stp _3stn"})
         all_favorites = table_favorites.find_all("div", {"class": "labelContainer"})
         page_name = table_favorites.find_all("div", {"class": "mediaPageName"})
 
-        print(name)
-        profile["name"] = name
-        print("\t\t Work")
-        profile["work"] = []
-        for i in range(0, len(work)):
-            print(work[i].a.get_text())
-            profile["work"].append(work[i].a.get_text())
-        print("\t\t Study")
-        profile["study"] = []
-        for i in range(0, len(study)):
-            print(study[i].a.get_text())
-            profile["study"].append(study[i].a.get_text())
-        print("\n\n\t\tWas born in: " + cities[1].a.get_text() + " - Living in: " + cities[0].a.get_text())
-        profile["cities"] = []
-        for i in range(0, len(cities)):
-            profile["cities"].append(cities[i].a.get_text())
         for i in range(0, len(all_favorites)-1):
             titles.append(all_favorites[i].get_text())
             contents.append(page_name[i].get_text())
